30403bd850283db1e1be2c246a6e4e6207f85f34
Eric Nantz <thercast@gmail.com>
2024-10-29 13:16:05 +0000

    try options method for auth0 toggle

--- R/run_app.R
+++ R/run_app.R
@@ -1,5 +1,5 @@
 #' Run the Shiny Application
-#'
+#' @param use_auth0 toggle to use auth0 version of app. Default is TRUE.
 #' @param ... arguments to pass to golem_opts.
 #' See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
@@ -12,18 +12,34 @@ run_app <- function(
   options = list(),
   enableBookmarking = NULL,
   uiPattern = "/",
+  use_auth0 = TRUE,
   ...
 ) {
-  with_golem_options(
-    app = auth0::shinyAppAuth0(
-      ui = app_ui(),
-      server = app_server,
-      onStart = onStart,
-      options = options,
-      enableBookmarking = enableBookmarking,
-      uiPattern = uiPattern,
-      config_file = system.file('app/_auth0.yml', package = 'rph2024.haunted')
-    ),
-    golem_opts = list(...)
-  )
+  if (use_auth0) {
+    with_golem_options(
+      app = auth0::shinyAppAuth0(
+        ui = app_ui(),
+        server = app_server,
+        onStart = onStart,
+        options = options,
+        enableBookmarking = enableBookmarking,
+        uiPattern = uiPattern,
+        config_file = system.file('app/_auth0.yml', package = 'rph2024.haunted')
+      ),
+      golem_opts = list(...)
+    )
+  } else {
+    with_golem_options(
+      app = shiny::shinyApp(
+        ui = app_ui,
+        server = app_server,
+        onStart = onStart,
+        options = options,
+        enableBookmarking = enableBookmarking,
+        uiPattern = uiPattern
+      ),
+      golem_opts = list(...)
+    )
+  }
+  
 }
