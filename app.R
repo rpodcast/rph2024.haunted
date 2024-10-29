5a8d3b396c971d8a192bf14a680ac46bd54f2e5a
Eric Nantz <thercast@gmail.com>
2024-10-29 13:08:19 +0000

    disable auth0 for posit connect version of app

--- app.R
+++ app.R
@@ -5,6 +5,6 @@
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 options("shiny.port" = 2556)
-options(auth0_disable = FALSE)
+options(auth0_disable = TRUE)
 options(auth0_config_file = "inst/app/_auth0.yml")
 run_app() # add parameters here (if any)
